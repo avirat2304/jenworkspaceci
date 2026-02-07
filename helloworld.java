6c04580c2d6a4497bc81ba1e6cc77be5675863c2
avirat.patil <avirat.patil@zeuslearning.com>
2026-02-07 12:15:10 +0530

    adding readme and java files

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Fun Fact 🤖: Jenkins was originally called 'Hudson' and was renamed in 2011!");
+        System.out.println("Another Fun Fact 🚀: Jenkins can run thousands of jobs automatically while you enjoy your coffee ☕.");
+    }
+}
